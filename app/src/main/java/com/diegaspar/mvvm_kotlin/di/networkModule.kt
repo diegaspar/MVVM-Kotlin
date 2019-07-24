c7c2150cae49f00136aa5ce71e658b731cd4b6e8
diegogaspar <diegaspar@gmail.com>
2019-07-24 10:19:21 +0200

    Added UnitTesting for the ViewModel

--- app/src/main/java/com/diegaspar/mvvm_kotlin/di/networkModule.kt
+++ app/src/main/java/com/diegaspar/mvvm_kotlin/di/networkModule.kt
@@ -1,9 +1,7 @@
 package com.diegaspar.mvvm_kotlin.di
 
 import com.google.gson.GsonBuilder
-import okhttp3.Cache
 import okhttp3.OkHttpClient
-import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -13,13 +11,11 @@ import java.util.concurrent.TimeUnit
 private const val CONNECT_TIMEOUT = 10L
 
 val networkModule = module {
-    single { Cache(androidApplication().cacheDir, 10L * 1024 * 1024) }
 
     single { GsonBuilder().create() }
 
     single {
         OkHttpClient.Builder().apply {
-            cache(get())
             connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
             retryOnConnectionFailure(true)
         }.build()
---
+++ app/src/test/java/com/diegaspar/mvvm_kotlin/MainViewModelTest.kt
@@ -0,0 +1,88 @@
+package com.diegaspar.mvvm_kotlin
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.diegaspar.mvvm_kotlin.model.PostsRepository
+import com.diegaspar.mvvm_kotlin.model.persistence.PostDB
+import com.diegaspar.mvvm_kotlin.viewmodel.MainViewModel
+import com.google.common.truth.Truth
+import io.reactivex.Observable
+import io.reactivex.observers.TestObserver
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNull
+import org.junit.Rule
+import org.junit.Test
+
+
+class MainViewModelTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    @Rule
+    @JvmField
+    var testSchedulerRule = RxImmediateSchedulerRule()
+
+    private val repoMocked = mock<PostsRepository>()
+    private val viewModel by lazy { MainViewModel(repoMocked) }
+    private val networkError = "NetworkError"
+
+    private val postList = listOf(
+        PostDB(0, 1, "Post1Title", "Post1Body"),
+        PostDB(1, 1, "Post2Title", "Post2Body")
+    )
+
+    @Test
+    fun loadPost_response_okay() {
+        whenever(repoMocked.getPosts()).thenReturn(Observable.just(postList))
+        val liveDataUnderTest = testLiveDataPost()
+
+        Truth.assert_()
+            .that(liveDataUnderTest.observedValues)
+            .isEqualTo(emptyList<PostDB>())
+        viewModel.loadPosts()
+        assertNull(this.viewModel.posts.value)
+
+        val result = repoMocked.getPosts()
+        val testObserver = TestObserver<List<PostDB>>()
+        assertEquals(true, viewModel.isLoading.get())
+
+        result.subscribe(testObserver)
+        testObserver.assertComplete()
+        testObserver.assertNoErrors()
+
+        result.doOnNext {
+            Truth.assert_()
+                .that(liveDataUnderTest.observedValues)
+                .isEqualTo(listOf(postList))
+        }
+
+        result.doOnComplete {
+            assertEquals(false, viewModel.isLoading.get())
+        }
+    }
+
+
+    @Test
+    fun loadPost_response_error() {
+        whenever(repoMocked.getPosts()).thenReturn(Observable.error(Error(networkError)))
+        val liveDataUnderTest = viewModel.posts.testObserver()
+
+        Truth.assert_()
+            .that(liveDataUnderTest.observedValues)
+            .isEqualTo(emptyList<PostDB>())
+        viewModel.loadPosts()
+        assertNull(this.viewModel.posts.value)
+
+        val result = repoMocked.getPosts()
+        val testObserver = TestObserver<List<PostDB>>()
+
+        result.subscribe(testObserver)
+        testObserver.assertErrorMessage(networkError)
+    }
+
+
+    private fun testLiveDataPost(): com.diegaspar.mvvm_kotlin.TestObserver<ArrayList<PostDB>> {
+        return viewModel.posts.testObserver()
+    }
+}
